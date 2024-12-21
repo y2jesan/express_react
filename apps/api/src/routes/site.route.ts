3cd7a51987c830f56eddd186edb0ad27890dd431
y2jesan <ymjesanlemon@gmail.com>
2024-12-21 23:46:56 +0600

    dynamic data

--- apps/api/src/routes/site.route.ts
+++ apps/api/src/routes/site.route.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Home', layout: '../layouts/site-layout' });
+  res.render('index', { title: 'Qtec Solutions', layout: '../layouts/site-layout' });
 });
 
 export default router;
